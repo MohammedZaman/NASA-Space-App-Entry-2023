132ed877c5d6e1dc46362958257ee3d4d33639d5
mohammedzaman <mohamedsaifuz@gmail.com>
2023-10-08 12:48:55 +0100

    Working version of the map

--- js/map.js
+++ js/map.js
@@ -1,142 +1,53 @@
-let map;
+var long
+var lat
 
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: {
-            lat: -28,
-            lng: 137
-        },
-        styles: [
-            {
-                elementType: "geometry",
-                stylers: [{
-                    color: "#242f3e"
-                }]
-            },
-            {
-                elementType: "labels.text.stroke",
-                stylers: [{
-                    color: "#242f3e"
-                }]
-            },
-            {
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#746855"
-                }]
-            },
-            {
-                featureType: "administrative.locality",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#d59563"
-                }],
-      },
-            {
-                featureType: "poi",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#d59563"
-                }],
-      },
-            {
-                featureType: "poi.park",
-                elementType: "geometry",
-                stylers: [{
-                    color: "#263c3f"
-                }],
-      },
-            {
-                featureType: "poi.park",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#6b9a76"
-                }],
-      },
-            {
-                featureType: "road",
-                elementType: "geometry",
-                stylers: [{
-                    color: "#38414e"
-                }],
-      },
-            {
-                featureType: "road",
-                elementType: "geometry.stroke",
-                stylers: [{
-                    color: "#212a37"
-                }],
-      },
-            {
-                featureType: "road",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#9ca5b3"
-                }],
-      },
-            {
-                featureType: "road.highway",
-                elementType: "geometry",
-                stylers: [{
-                    color: "#746855"
-                }],
-      },
-            {
-                featureType: "road.highway",
-                elementType: "geometry.stroke",
-                stylers: [{
-                    color: "#1f2835"
-                }],
-      },
-            {
-                featureType: "road.highway",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#f3d19c"
-                }],
-      },
-            {
-                featureType: "transit",
-                elementType: "geometry",
-                stylers: [{
-                    color: "#2f3948"
-                }],
-      },
-            {
-                featureType: "transit.station",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#d59563"
-                }],
-      },
-            {
-                featureType: "water",
-                elementType: "geometry",
-                stylers: [{
-                    color: "#17263c"
-                }],
-      },
-            {
-                featureType: "water",
-                elementType: "labels.text.fill",
-                stylers: [{
-                    color: "#515c6d"
-                }],
-      },
-            {
-                featureType: "water",
-                elementType: "labels.text.stroke",
-                stylers: [{
-                    color: "#17263c"
-                }],
-      },
-    ],
+document.addEventListener("DOMContentLoaded", function () {
+    initGeolocation()
+    mapboxgl.accessToken = 'pk.eyJ1IjoibW9oYW1tZWR6YW1hbiIsImEiOiJjbG5nbjJjYXIwdWF1Mm1ybGZ4aTB3OW5oIn0.S0FeiTca-v5oQWuWvhRT4g';
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mohammedzaman/clngnk2ry040001qx1sie0uvb/draft',
+        center: getlocation(),
+        zoom: 4
     });
-    // NOTE: This uses cross-domain XHR, and may not work on older browsers.
-    map.data.loadGeoJson(
-        "https://storage.googleapis.com/mapsdevsite/json/google.json",
-    );
+});
+
+
+
+function getlocation() {
+    if (long != null && lat != null) {
+        // Using user location 
+        return new Array(long, lat);
+    } else {
+        // United Kingdom long and lat
+        return new Array(-3.767490, 53.711949); // [lng, lat]
+    }
+
 }
 
-window.initMap = initMap;
+
+function initGeolocation() {
+    if (navigator.geolocation) {
+        // Call getCurrentPosition with success and failure callbacks
+        navigator.geolocation.getCurrentPosition(success, fail);
+
+    } else {
+        alert("Sorry, your browser does not support geolocation services.");
+    }
+}
+
+function success(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
+
+    document.getElementById('long').innerHTML = "Longitude: " + long;
+    document.getElementById('lat').innerHTML = "Latitude:" + lat
+
+}
+
+function fail() {
+    alert("Unable to get your location. The default location is the UK")
+}
